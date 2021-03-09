4a99dee0e663d4916219bacca795e32d8fd22b0e
Ritter Gustave <rGustave5@gmail.com>
2021-03-08 21:57:17 -0800

    db into main: database implemented (#4)
    * chore(app): connected db with environment variables
    
    * feat:(players): added C and R part of crud to player routes
    
    * feat(users): added password hashing and ability to add users to db
    
    Co-authored-by: Ritter Gustave <ritter.gustave@sureprep.com>

--- src/app/controllers/userController.go
+++ src/app/controllers/userController.go
@@ -1,17 +1,63 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 
+	model "fbhc.com/api/main/models/users"
 	"gorm.io/gorm"
 )
+
+// UnmarshalUserJSON into golang struct
+func UnmarshalUserJSON(incData []byte) (*model.User, error) {
+	var data *model.User
+	err := json.Unmarshal(incData, &data)
+
+	return data, err
+}
 
 // AddNewUser does stuff
 func AddNewUser(db *gorm.DB) http.HandlerFunc {
 	// Maybe middleware here?
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("userController activated")
+
+		request, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+
+		userData, err := UnmarshalUserJSON(request)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		userData.CreatedAt = time.Now()
+		userData.UpdatedAt = time.Now()
+
+		_, err = userData.HashOwnPassword()
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		db.Select(
+			"FirstName",
+			"LastName",
+			"Email",
+			"Password",
+			"CreatedAt",
+			"UpdatedAt",
+		).Create(userData)
+
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
@@ -22,24 +68,3 @@ func GetUsers(db *gorm.DB) http.HandlerFunc {
 		fmt.Println("userController activated")
 	}
 }
-
-// GetRoutes retrieves routes
-// func GetUserRoutes(database *sql.DB) []routing.Route {
-
-// 	db.CreateTable(
-// 		database,
-// 		"users",
-// 		`"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT`,
-// 		`"firstname" TEXT`,
-// 		`"lastname" TEXT`,
-// 		`"email" integer`,
-// 		`"password" string`,
-// 	)
-
-// 	playerRoutes := []routing.Route{
-// 		{
-// 			Path:    "/users",
-// 			Method:  "GET",
-// 			Handler: addNewUser(database),
-// 		},
-// 	}
