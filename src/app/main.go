448678504a02bd9c0717f48c7b890ba007206266
Ritter Gustave <rGustave5@gmail.com>
2021-03-11 21:01:01 -0800

    Restruct (#5)
    * chore(app): connected db with environment variables
    
    * feat:(players): added C and R part of crud to player routes
    
    * feat(users): added password hashing and ability to add users to db
    
    * feat(application): Complete reorganization and architecture philosophy
    
    Co-authored-by: Ritter Gustave <ritter.gustave@sureprep.com>

--- src/app/main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	application := App{}
-	application.Initialize().Run(":8080")
-}
--- src/app/utility/hashing.go
+++
@@ -1,9 +0,0 @@
-package utility
-
-import "golang.org/x/crypto/bcrypt"
-
-// HashString HashString hashes the given string.
-func HashString(str string) (string, error) {
-	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(str), bcrypt.DefaultCost)
-	return string(hashedBytes), err
-}
