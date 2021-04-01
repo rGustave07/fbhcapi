8df53c7aaa35a33a99c2d234180fd4e0dcf2270f
Ritter Gustave <ritter.gustave@sureprep.com>
2021-04-01 00:03:11 -0700

    feat(service/repository): worked on added repository and service layers

--- api/service/user_service.go
+++ api/service/user_service.go
@@ -3,8 +3,35 @@ package service
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/rGustave07/fbhcapi/api/model"
 )
+
+// UserService acts as a struct for injecting an implementation of
+// UserRepository for use in service methods
+type UserService struct {
+	UserRepository model.UserRepository
+}
+
+// USConfig will hold repositories that will eventually be injected
+// into this service layer
+type USConfig struct {
+	UserRepository model.UserRepository
+}
+
+// NewUserService is a factor function for
+// initializing a UserService with its repository layer dependencies
+func NewUserService(c *USConfig) model.UserService {
+	return &UserService{
+		UserRepository: c.UserRepository,
+	}
+}
+
+func (s *UserService) Get(ctx context.Context, uid uuid.UUID) (*model.User, error) {
+	u, err := s.UserRepository.FindByID(ctx, uid)
+
+	return u, err
+}
 
 func (s *UserService) Signup(ctx context.Context, u *model.User) error {
 	panic("Method not implemented")
